95646386b808dd701de61a717ffb713fe2192f4f
SamarV-121 <samarvispute121@pm.me>
2023-06-25 14:26:46 +0200

    addons: Add brightness slider styles [3/3]
    Author: rdx420 <padraramesh420@gmail.com>
    Date:   Sat May 27 23:12:57 2023 +0200
    
        addons: Add Translucent brightness slider styles
    
        Credit - https://github.com/Mahmud0808/Iconify
    
    Co-authored-by: rdx420 <padraramesh420@gmail.com>
    Co-authored-by: DrDisagree <crazymahmud08@gmail.com>
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- config.mk
+++ config.mk
@@ -21,6 +21,12 @@ PRODUCT_COPY_FILES += \
     vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2022-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2022-and-newer.xml \
     vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2023-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2023-and-newer.xml
 
+# Brightness slider styles
+PRODUCT_PACKAGES += \
+    BrightnessSliderFilledOverlay \
+    BrightnessSliderThinOverlay \
+    BrightnessSliderTranslucentOverlay
+
 # Clocks (SystemUI)
 PRODUCT_PACKAGES += \
     SystemUIClocks-BigNum \
