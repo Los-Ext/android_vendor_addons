cfe34c21ff78a876e5bde57af0305ec639143756
StarkDroid <gamerprince.exp@gmail.com>
2023-11-24 15:27:39 +0530

    addons: Add A11 QS UI Style [2/3]
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -145,6 +145,11 @@ PRODUCT_PACKAGES += \
     FontRosemaryOverlay \
     FontUbuntuOverlay
 
+
+# QS UI Style
+PRODUCT_PACKAGES += \
+    A11QSUI
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
