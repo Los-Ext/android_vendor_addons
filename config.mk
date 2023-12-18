6fa9de81f4f158bcd89c33d0be5b5f24436d0623
Pranav Vashi <neobuddy89@gmail.com>
2023-12-18 21:23:19 +0530

    addons: Add IconShapes
    Co-authored-by: SagarMakhar <sagarmakhar@gmail.com>
    Co-authored-by: ROGERdotT <roger.truttmann35@gmail.com>
    Co-authored-by: Manish4586 <Manish.n.manish45@gmail.com>
    Co-authored-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- config.mk
+++ config.mk
@@ -225,6 +225,25 @@ PRODUCT_PACKAGES += \
     IconPackXperiaSettingsOverlay \
     IconPackXperiaSystemUIOverlay
 
+# Icon Shapes
+PRODUCT_PACKAGES += \
+    IconShapeCloudyOverlay \
+    IconShapeCylinderOverlay \
+    IconShapeFlowerOverlay \
+    IconShapeHeartOverlay \
+    IconShapeHexagonOverlay \
+    IconShapeLeafOverlay \
+    IconShapeMeowOverlay \
+    IconShapePebbleOverlay \
+    IconShapeRoundedHexagonOverlay \
+    IconShapeRoundedRectOverlay \
+    IconShapeSquareOverlay \
+    IconShapeSquircleOverlay \
+    IconShapeStretchedOverlay \
+    IconShapeTaperedRectOverlay \
+    IconShapeTeardropOverlay \
+    IconShapeVesselOverlay
+
 # QS UI Style
 PRODUCT_PACKAGES += \
     A11QSUI
