8a741bdb0c33f6ca9b90a9cbaf37493b3d1907f1
mukesh22584 <mks22584@gmail.com>
2024-02-11 19:22:46 +0530

    addons: add Ios font for lockscreen Clock

--- config.mk
+++ config.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
     ClockFontHangedOverlay \
     ClockFontHarmonySansOverlay \
     ClockFontHotSweatOverlay \
+    ClockFontIosOverlay \
     ClockFontKGOnlyHopeOverlay \
     ClockFontKaramuruhOverlay \
     ClockFontKingthingsOverlay \
