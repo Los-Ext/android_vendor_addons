6f932856b6c9711b79516de4f7510259ffa30c25
ShevT <evgeny.shishkov@gmail.com>
2024-04-24 10:29:22 +0300

    addons: Add Play font package
    https: //fonts.google.com/specimen/Play?query=play
    
    Change-Id: Ic09673f39bc9678b397a7cf835a3a8e2745d9271

--- config.mk
+++ config.mk
@@ -69,6 +69,7 @@ PRODUCT_PACKAGES += \
     ClockFontPinewoodOverlay \
     ClockFontPlaidEventOverlay \
     ClockFontPlantsLettersOverlay \
+    ClockFontPlayOverlay \
     ClockFontQuandoOverlay \
     ClockFontQuickSouthOverlay \
     ClockFontRedressedOverlay \
@@ -116,6 +117,7 @@ PRODUCT_PACKAGES += \
     FontOneplusSansOverlay \
     FontOneplusSlateOverlay \
     FontOswaldOverlay \
+    FontPlayOverlay \
     FontQuandoOverlay \
     FontRedressedOverlay \
     FontReemKufiOverlay \
