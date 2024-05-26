023593f0d5ade91fd37793e58f8d83e2306680ed
Naveen Johnson <14140949+naveenjohnsonv@users.noreply.github.com>
2024-05-27 02:34:59 +0900

    addons: Add OneUISans fonts
    Change-Id: I46c6cb55e36546a40b36da76a7014be2151a876c

--- config.mk
+++ config.mk
@@ -128,6 +128,7 @@ PRODUCT_PACKAGES += \
     FontNothingDotHeadlineOverlay \
     FontNothingDotOverlay \
     FontNunitoOverlay \
+    FontOneUISansOverlay \
     FontOneplusSansOverlay \
     FontOneplusSlateOverlay \
     FontOswaldOverlay \
