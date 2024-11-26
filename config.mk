299126a37fb73808d8657eff7ca018aace74c69b
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-26 20:13:34 +0800

    addons: Add MotoMilkyStacked font
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
     ClockFontSlimOverlay \
     ClockFontNtype82Overlay \
     ClockFontSubwayOverlay \
+    ClockFontMotorola \
     FontAccuratistOverlay \
     FontAclonicaOverlay \
     FontAmaranteOverlay \
