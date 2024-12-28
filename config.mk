cc8e083c4bda711d07fa6a4ae0d1f84de61d2c51
Tejas Singh <tejassingh649@rediffmail.com>
2024-12-28 22:02:11 +0000

    addons: properties: Set default game props

--- config.mk
+++ config.mk
@@ -312,3 +312,6 @@ $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/addons/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+# GameProps
+include vendor/addons/gameprops/game_props.mk
