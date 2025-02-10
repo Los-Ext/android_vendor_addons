10feec70129b05a25c3bab7272cc4c8a7db9d039
minaripenguin <minaripenguin@users.noreply.github.com>
2025-02-10 18:11:46 +0900

    extras: overlays: Add overlay for hiding smartspace view and adding offset
    * for custom clocks
    
    Change-Id: I7cdf0b8925429f48716f3878a221ccf2e4305197
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -339,6 +339,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageBlackTheme
 
+# Utility Overlays
+PRODUCT_PACKAGES += \
+    HideSmartSpace \
+    SmartSpaceOffset \
+    HideClock
+
 # Include {Lato,Rubik} fonts
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
