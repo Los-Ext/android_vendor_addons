e8f9a3ac77519f454e3abe5b5e651789d6203eaf
ImSpiDy <SpiDy2713@gmail.com>
2025-02-26 06:23:28 +0000

    addons: convert game props.mk into prop files
    - PRODUCT_PRODUCT_PROPERTIES was not able to set props if there was space in the value
    - It was moving the values in next line
    
    thnx tedomi2705 for informing about this issue
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- config.mk
+++ config.mk
@@ -347,4 +347,4 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/addons/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 
 # GameProps
-include vendor/addons/gameprops/game_props.mk
+TARGET_PRODUCT_PROP += vendor/addons/gameprops/game.prop
