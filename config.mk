5e7fe9d9b3d87b4a38e0201ca1e3190496a672ed
Ghosuto <clash.raja10@gmail.com>
2025-08-10 06:03:27 +0000

    addons: Add Simpleday font
    Change-Id: I6f8f97fb346212589977a26bd87f2e8c2d1c7993
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- config.mk
+++ config.mk
@@ -171,6 +171,7 @@ PRODUCT_PACKAGES += \
     FontSanFranciscoDisplayProSourceOverlay \
     FontSamsungOneOverlay \
     FontSansSerifOverlay \
+    FontSimpleDayOverlay \
     FontSonySketchOverlay \
     FontStoropiaOverlay \
     FontSurferOverlay \
