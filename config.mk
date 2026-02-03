e3e4954cd16b22041c269e334f6001dc55508006
ImSpiDy <SpiDy2713@gmail.com>
2026-02-03 20:43:48 +0000

    addons: Don't Inherit Game Props for now
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- config.mk
+++ config.mk
@@ -335,4 +335,4 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/addons/prebuilt/product/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 
 # GameProps
-TARGET_PRODUCT_PROP += vendor/addons/gameprops/game.prop
+# TARGET_PRODUCT_PROP += vendor/addons/gameprops/game.prop
